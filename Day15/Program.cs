8b312f4f805ad1d3a9dcde1bb39081dab4cff4bd
Christian <cwaters@truenorthit.co.uk>
2022-12-15 14:49:25 +0000

    tidy up

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Day15
 {
@@ -8,33 +9,25 @@ namespace Day15
         const int MAX = 4000000;
         static void Main(string[] args)
         {
-            var a2 = part2();
-            var a2answer = ((long) a2.Item1) * 4000000 + a2.Item2;
-        }
-
-        static (int,int) part2()
-        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
             var coords = File.ReadAllLines("input.txt")
                 .Select(str => str.Split(": "))
                 .Select(arr => (arr[0][10..].Split(", "), arr[1][21..].Split(", ")))
                 .Select(tp => (tp.Item1.Select(str => int.Parse(str[2..])).ToArray(), tp.Item2.Select(str => int.Parse(str[2..])).ToArray()))
                 .Select(tp => (tp.Item1[0], tp.Item1[1], tp.Item2[0], tp.Item2[1]))
                 .ToArray<(int sx, int sy, int bx, int by)>();
-            
-            var impossible = new Dictionary<int, HashSet<(int, int)>>();
+
+            var ranges = new Dictionary<int, HashSet<(int, int)>>();
+
             //add all beacons and sensors as impossible positions
-            foreach ((int sx, int sy, int bx, int by) in coords)
+            foreach ((int x, int y) in coords.SelectMany(tp => new[] { (tp.sx, tp.sy), (tp.bx, tp.by) }))
             {
-                if (!impossible.ContainsKey(by))
+                if (!ranges.ContainsKey(y))
                 {
-                    impossible[by] = new HashSet<(int, int)>();
+                    ranges[y] = new HashSet<(int, int)>();
                 }
-                impossible[by].Add((bx, bx));
-                if (!impossible.ContainsKey(sy))
-                {
-                    impossible[sy] = new HashSet<(int, int)>();
-                }
-                impossible[sy].Add((sx, sx));
+                ranges[y].Add((x, x));
             }
 
             foreach (var coord in coords)
@@ -48,109 +41,50 @@ namespace Day15
                     var x1 = coord.sx - dx;
                     var x2 = coord.sx + dx;
 
-                    //exclude the range x1 => x2
+                    //exclude the range [x1..x2]
                     //we have to process any ranges currently in the list
-
-                    if (!impossible.ContainsKey(y))
+                    if (!ranges.ContainsKey(y))
                     {
-                        impossible[y] = new HashSet<(int, int)>();
+                        ranges[y] = new HashSet<(int, int)>();
                     }
                     var newRanges = new HashSet<(int, int)>();
-                    foreach ((int rx1, int rx2) in impossible[y])
+                    foreach ((int rx1, int rx2) in ranges[y])
                     {
-                        //merge these
-                        if (x1 == rx2 + 1)
+                        if (x1 <= rx2 + 1 && x2 >= rx1 - 1)
                         {
-                            x1 = rx1;
-                        }
-                        else if (x2 == rx1 - 1)
-                        {
-                            x2 = rx2;
-                        }
-                        //seperate
-                        else if (x2 < rx1 || x1 > rx2)
-                        {
-                            newRanges.Add((rx1, rx2));
-                        }
-                        //fully enclosed
-                        else if (x1 >= rx1 && x2 <= rx2)
-                        {
-                            (x1, x2) = (rx1, rx2);
-                        }
-                        //fully encloses
-                        else if (x1 <= rx1 && x2 >= rx2)
-                        {                            
-                        }
-                        //start overlaps
-                        else if (x1 < rx1)
-                        {
-                            x2 = rx2;
-                        }
-                        //end overlaps
-                        else if (x2 > rx2)
-                        {
-                            x1 = rx1;
+                            //can be merged
+                            (x1, x2) = (Math.Min(x1, rx1), Math.Max(x2, rx2));
                         }
                         else
                         {
-                            throw new Exception("BOOM!");
+                            //must be completely separate
+                            newRanges.Add((rx1, rx2));
                         }
                     }
 
                     newRanges.Add((x1, x2));
-                    impossible[y] = newRanges;
+                    ranges[y] = newRanges;
                 }
             }
 
             //find a row with a single possibility
-            var maybe = impossible.Where(kvp => kvp.Key >= 0 && kvp.Key <= MAX && kvp.Value.Count > 1).ToList();
-            foreach (var kvp in maybe)
-            {
-                for (int x = 0; x <= MAX; x++)
-                {
-                    //check row to see if there is a single possibility
-                    if (kvp.Value.All(tp => !(tp.Item1 <= x && tp.Item2 >= x)))
-                    {
-                        return (x, kvp.Key);
-                    }
-                }
-            }
+            var solution = ranges
+                .Where(kvp => kvp.Key >= 0 && kvp.Key <= MAX && kvp.Value.Count > 1).Single();
 
-            return (-1, -1);
-        }
-        static int part1()
-        {
-            var coords = File.ReadAllLines("input.txt")
-                .Select(str => str.Split(": "))
-                .Select(arr => (arr[0][10..].Split(", "), arr[1][21..].Split(", ")))
-                .Select(tp => (tp.Item1.Select(str => int.Parse(str[2..])).ToArray(), tp.Item2.Select(str => int.Parse(str[2..])).ToArray()))
-                .Select(tp => (tp.Item1[0], tp.Item1[1], tp.Item2[0], tp.Item2[1]))
-                .ToArray<(int sx, int sy, int bx, int by)>();
-            int R = 2000000;
+            //wouldn't work if the single possibility was right on the edge (x = 0 or x = MAX)
+            //but seems it always does :)
+            var answers =
+                solution.Value.SelectMany(tp => new[] { tp.Item1, tp.Item2 })
+                .Where(x => x >= 0 && x <= MAX)
+                .ToList();
 
-            HashSet<int> impossible = new HashSet<int>();
-            foreach (var coord in coords)
-            {
-                var d = Math.Abs(coord.sx - coord.bx) + Math.Abs(coord.sy - coord.by);
-                var dx = d - Math.Abs(coord.sy - R);
-                var x1 = coord.sx - dx;
-                var x2 = coord.sx + dx;
+            var part1 = ranges[2000000].Single().Item2 - ranges[2000000].Single().Item1;
+            var (p2x, p2y) = (Math.Abs((answers[1] + answers[0]) / 2), solution.Key);
+            var part2 = ((long)p2x) * 4000000 + p2y;
 
-                for (int x = x1; x <= x2; x++)
-                {
-                    impossible.Add(x);
-                }
-            }
-
-            foreach ((int sx, int sy, int bx, int by) in coords)
-            {
-                if (by == R && impossible.Contains(bx))
-                {
-                    impossible.Remove(bx);
-                }
-            }
-
-            return impossible.Count;
+            Console.WriteLine($"Part 1: {part1}");
+            Console.WriteLine($"Part 2: {part2}");
+            Console.WriteLine($"Time taken: {watch.ElapsedMilliseconds}ms");
         }
     }
 }
