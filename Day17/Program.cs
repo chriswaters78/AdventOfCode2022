de4757c2e87812e4567820a542dcbc326a14132c
Christian <cwaters@truenorthit.co.uk>
2022-12-17 15:46:52 +0000

    part 1

---
+++ Day17/Program.cs
@@ -0,0 +1,145 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Day17
+{
+    internal class Program
+    {
+        //####
+
+        //.#.
+        //###
+        //.#.
+
+        //..#
+        //..#
+        //###
+
+        //#
+        //#
+        //#
+        //#
+
+        //##
+        //##
+
+        static (int ox, int oy)[][] shapes = new[] {
+                new [] {(0,0), (1,0), (2,0), (3,0) },
+                new [] {(0,1), (1,0), (1,1), (1,2), (2,1) },
+                new [] {(0,0), (1,0), (2,0), (2,1), (2,2)},
+                new [] {(0,0), (0,1), (0,2), (0,3)},
+                new [] {(0,0), (0,1), (1,0), (1,1)},
+            };
+
+        static HashSet<(int x, int y)> rocks = new HashSet<(int x, int y)>();
+
+        const int MAXY = 5000;
+        
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            
+            var jets = File.ReadAllText($"{args[0]}.txt").Select(ch => ch).ToArray();
+
+            for (int x = 0; x < 9; x++)
+            {
+                rocks.Add((x, 0));
+            }
+            for (int y = 0; y < MAXY; y++)
+            {
+                rocks.Add((0, y));
+                rocks.Add((8, y));
+            }
+
+            int tallestRock = 0;
+
+
+            int j = 0;
+            for (int r = 0; r < 2022; r++)
+            {
+                Console.WriteLine(print());
+
+                int currentX = 3;
+                int currentY = tallestRock + 4;
+
+                while (true)
+                {
+                    //try blow with jet
+                    var nx = currentX + (jets[j % jets.Length] == '>' ? 1 : -1);
+                    if (!anyHit(nx, currentY, r))
+                    {
+                        currentX = nx;
+                    }
+                    j++;
+
+                    //now try and move down
+                    var ny = currentY - 1;
+                    if (!anyHit(currentX, ny, r))
+                    {
+                        currentY = ny;
+                    }
+                    else
+                    {
+                        goto stopped;
+                    }
+
+                }
+            stopped:;
+                foreach ((int ox, int oy) in shapes[r % shapes.Length])
+                {
+                    rocks.Add((currentX + ox, currentY + oy));
+                    if (currentY + oy > tallestRock)
+                    {
+                        tallestRock = currentY + oy;
+                    }
+                }
+            }
+
+            var part1 = tallestRock;
+            Console.WriteLine($"Part1: {part1} in {watch.ElapsedMilliseconds}ms");
+        }
+
+        private static string print()
+        {
+            var maxY = rocks.Where(tp => tp.x > 0 && tp.x < 8).Max(tp => tp.y);
+
+            List<StringBuilder> rows = new List<StringBuilder>();
+            var sb = new StringBuilder();
+            sb.AppendLine("+-------+");
+            rows.Add(sb);
+            for (int y = 1; y <= maxY; y++)
+            {
+                var sb2 = new StringBuilder();
+                sb2.AppendLine("|.......|");
+                rows.Add(sb2);
+            }
+
+            foreach (var rock in rocks)
+            {
+                if (rock.y > 0 && rock.y <= maxY)
+                {
+                    rows[rock.y][rock.x] = '#';
+                }
+            }
+
+            return String.Join("", rows.Select(sb => sb.ToString()).Reverse());
+        }
+
+        private static bool anyHit(int x, int y, int r)
+        {
+            foreach ((int ox, int oy) in shapes[r % shapes.Length])
+            {
+                var next = (x + ox, y + oy);
+                if (rocks.Contains(next))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
