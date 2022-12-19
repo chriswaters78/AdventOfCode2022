fadd5648ce56944f4a13ec9d59cca80b5cdbb843
Christian <cwaters@truenorthit.co.uk>
2022-12-19 21:39:11 +0000

    part 1 and part 2

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Day19
 {
     internal class Program
     {
-        const int MINUTES = 32;
+        static int MINUTES;
         //ore, clay, obsidian, geode
         static List<List<((int ore, int clay, int obsidian) costs, (int ore, int clay, int obsidian, int geode) produces)>> blueprints;
         static Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int> state;
         static void Main(string[] args)
         {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            
             blueprints = File.ReadAllLines($"{args[0]}.txt")
                 .Select(str => str.Split(' '))
                 .Select(
@@ -22,29 +26,34 @@ namespace Day19
                          ((int.Parse(arr[27]),0, int.Parse(arr[30])), (0,0,0,1)),
                      }).ToList();
 
+            MINUTES = 24;
             List<(int, int)> answers = new List<(int, int)>();
-
-            //for (int i = 1; i <= blueprints.Count; i++)
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i <= blueprints.Count; i++)
             {
-                Console.WriteLine($"Testing blue print {i}");
-                best = 0;
                 state = new Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int>();
-
                 var answer = solve(i - 1, 1, (0, 0, 0, 0), (1, 0, 0, 0));
-                Console.WriteLine($"Best was {answer}");
+                Console.WriteLine($"Blueprint {i}: {answer}, state count {state.Count} in {watch.ElapsedMilliseconds}ms");
                 answers.Add((i, answer));
             }
 
             var part1 = answers.Sum(tp => tp.Item1 * tp.Item2);
-            //Console.WriteLine($"Part 1: {part1}, state count {state.Count}");
+            Console.WriteLine($"Part 1: {part1}, elapsed {watch.ElapsedMilliseconds}ms");
+
+            watch.Restart();
+            MINUTES = 32;
+            answers = new List<(int, int)>();            
+            for (int i = 1; i <= 3; i++)
+            {
+                state = new Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int>();
+                var answer = solve(i - 1, 1, (0, 0, 0, 0), (1, 0, 0, 0));
+                Console.WriteLine($"Blueprint {i}: {answer}, state count {state.Count} in {watch.ElapsedMilliseconds}ms");
+                answers.Add((i, answer));
+            }
 
             var part2 = answers.Select(tp => tp.Item2).Aggregate(1, (int acc, int a) => acc * a);
-            Console.WriteLine($"Part 2: {part2}, state count {state.Count}");
+            Console.WriteLine($"Part 2: {part2}, elapsed {watch.ElapsedMilliseconds}ms");
         }
 
-        static int best;
-        //return max geodes
         static int solve(int bi, int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production)
         {
             if (state.ContainsKey((time, materials, production)))
@@ -52,38 +61,32 @@ namespace Day19
                 return state[(time, materials, production)];
             }
             
-            int maxGeode = 0;
             (int ore, int clay, int obsidian, int geode) materialsNext = (materials.ore + production.ore, materials.clay + production.clay, materials.obsidian + production.obsidian, materials.geode + production.geode);
             if (time == MINUTES)
             {
-                if (materialsNext.Item4 > best)
-                {
-                    Console.WriteLine($"New best {best}");
-                    best = materialsNext.Item4;
-                }
-                return materialsNext.Item4;
+                return materialsNext.geode;
             }
 
-            //foreach (var blueprint in blueprints[bi])
-            for (int bii = blueprints[bi].Count - 1; bii >= 0; bii--)
+            int maxGeode = 0;
+            foreach (var blueprint in blueprints[bi])
             {
-                var blueprint = blueprints[bi][bii];
                 if (materials.ore >= blueprint.costs.ore && materials.clay >= blueprint.costs.clay && materials.obsidian >= blueprint.costs.obsidian)
                 {
-                    //we could build one
-                    var productionNext = (production.ore + blueprint.produces.ore, production.clay + blueprint.produces.clay, production.obsidian + blueprint.produces.obsidian, production.geode + blueprint.produces.geode);
-                    var materialsNextMinusCost = (materialsNext.ore - blueprint.costs.ore, materialsNext.clay - blueprint.costs.clay, materialsNext.obsidian - blueprint.costs.obsidian, materialsNext.geode);
-                    maxGeode = Math.Max(maxGeode, solve(bi, time + 1, materialsNextMinusCost, productionNext));
-
-                    if (blueprint.produces.geode > 0)
-                    {
-                        goto done;
-                    }
+                    maxGeode = Math.Max(maxGeode, 
+                        solve(bi, time + 1,
+                        (   materialsNext.ore - blueprint.costs.ore, 
+                            materialsNext.clay - blueprint.costs.clay, 
+                            materialsNext.obsidian - blueprint.costs.obsidian, 
+                            materialsNext.geode), 
+                        (   production.ore + blueprint.produces.ore, 
+                            production.clay + blueprint.produces.clay, 
+                            production.obsidian + blueprint.produces.obsidian, 
+                            production.geode + blueprint.produces.geode)));
                 }
             }
 
-            maxGeode = Math.Max(maxGeode, solve(bi, time + 1, materialsNext, production));
-        done:;
+            maxGeode = Math.Max(maxGeode, 
+                solve(bi, time + 1, materialsNext, production));
             
             state[(time, materials, production)] = maxGeode;
             return maxGeode;
