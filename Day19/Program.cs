8ee97665203895ec7b6ee299fbb2c8c4cb3ff692
Christian <cwaters@truenorthit.co.uk>
2022-12-19 23:17:06 +0000

    refactoring

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -6,89 +6,78 @@ namespace Day19
 {
     internal class Program
     {
-        static int MINUTES;
-        //ore, clay, obsidian, geode
-        static List<List<((int ore, int clay, int obsidian) costs, (int ore, int clay, int obsidian, int geode) produces)>> blueprints;
-        static Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int> state;
+        record struct Material(int ore, int clay, int obsidian, int geode)
+        {
+            public Material Add(Material other) => new Material(ore + other.ore, clay + other.clay, obsidian + other.obsidian, geode + other.geode);
+            public Material Subtract(Material other) => new Material(ore - other.ore, clay - other.clay, obsidian - other.obsidian, geode - other.geode);
+        }
+
+        record struct State(int time, Material material, Material production);
+        
+        static List<List<(Material costs, Material produces)>> blueprintSets;
+
         static void Main(string[] args)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
             
-            blueprints = File.ReadAllLines($"{args[0]}.txt")
+            blueprintSets = File.ReadAllLines($"{args[0]}.txt")
                 .Select(str => str.Split(' '))
-                .Select(
-                    arr =>
-                     new List<((int ore, int clay, int obsidian) costs, (int ore, int clay, int obsidian, int geode) produces)>() {
-                         ((int.Parse(arr[6]), 0,0), (1,0,0,0)),
-                         ((int.Parse(arr[12]), 0,0), (0,1,0,0)),
-                         ((int.Parse(arr[18]), int.Parse(arr[21]),0), (0,0,1,0)),
-                         ((int.Parse(arr[27]),0, int.Parse(arr[30])), (0,0,0,1)),
-                     }).ToList();
+                .Select(    arr => new List<(Material costs, Material production)>() {
+                                     (new Material(int.Parse(arr[6]), 0, 0, 0), new Material(1,0,0,0)),
+                                     (new Material(int.Parse(arr[12]), 0, 0, 0), new Material(0,1,0,0)),
+                                     (new Material(int.Parse(arr[18]), int.Parse(arr[21]), 0, 0), new Material(0,0,1,0)),
+                                     (new Material(int.Parse(arr[27]),0, int.Parse(arr[30]), 0), new Material(0,0,0,1)),
+                            }).ToList();
 
-            MINUTES = 24;
-            List<(int, int)> answers = new List<(int, int)>();
-            for (int i = 1; i <= blueprints.Count; i++)
-            {
-                state = new Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int>();
-                var answer = solve(i - 1, 1, (0, 0, 0, 0), (1, 0, 0, 0));
-                Console.WriteLine($"Blueprint {i}: {answer}, state count {state.Count} in {watch.ElapsedMilliseconds}ms");
-                answers.Add((i, answer));
-            }
-
-            var part1 = answers.Sum(tp => tp.Item1 * tp.Item2);
+            var part1Answers = RunPart(24, blueprintSets);
+            var part1 = part1Answers.Sum(tp => tp.Item1 * tp.Item2);
             Console.WriteLine($"Part 1: {part1}, elapsed {watch.ElapsedMilliseconds}ms");
 
             watch.Restart();
-            MINUTES = 32;
-            answers = new List<(int, int)>();            
-            for (int i = 1; i <= 3; i++)
-            {
-                state = new Dictionary<(int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production), int>();
-                var answer = solve(i - 1, 1, (0, 0, 0, 0), (1, 0, 0, 0));
-                Console.WriteLine($"Blueprint {i}: {answer}, state count {state.Count} in {watch.ElapsedMilliseconds}ms");
-                answers.Add((i, answer));
-            }
-
-            var part2 = answers.Select(tp => tp.Item2).Aggregate(1, (int acc, int a) => acc * a);
+            var part2Answers = RunPart(32, blueprintSets.Take(3));
+            var part2 = part2Answers.Select(tp => tp.Item2).Aggregate(1, (int acc, int a) => acc * a);            
             Console.WriteLine($"Part 2: {part2}, elapsed {watch.ElapsedMilliseconds}ms");
         }
 
-        static int solve(int bi, int time, (int ore, int clay, int obsidian, int geode) materials, (int ore, int clay, int obsidian, int geode) production)
+        static List<(int index, int geodes)> RunPart(int MAXMINUTES, IEnumerable<List<(Material costs, Material produces)>> blueprintSets)
         {
-            if (state.ContainsKey((time, materials, production)))
+            var answers = new List<(int, int)>();
+            foreach ((var blueprints, int i) in blueprintSets.Select((bp, i) => (bp, i)))
             {
-                return state[(time, materials, production)];
+                var answer = Solve(MAXMINUTES, new Dictionary<State, int>(), blueprints, new State(1, new Material(0, 0, 0, 0), new Material(1, 0, 0, 0)));
+                Console.WriteLine($"Blueprint {i}: {answer}");
+                answers.Add((i + 1, answer));
             }
-            
-            (int ore, int clay, int obsidian, int geode) materialsNext = (materials.ore + production.ore, materials.clay + production.clay, materials.obsidian + production.obsidian, materials.geode + production.geode);
-            if (time == MINUTES)
+
+            return answers;
+        }
+
+        static int Solve(int MAXMINUTES, Dictionary<State, int> stateCache, List<(Material costs, Material production)> blueprints, State state)
+        {
+            if (stateCache.TryGetValue(state, out int result))
             {
-                return materialsNext.geode;
+                return result;
+            }
+
+            var nextState = state with { time = state.time + 1, material = state.material.Add(state.production) };
+            if (state.time == MAXMINUTES)
+            {
+                return nextState.material.geode;
             }
 
             int maxGeode = 0;
-            foreach (var blueprint in blueprints[bi])
+            foreach (var blueprint in blueprints)
             {
-                if (materials.ore >= blueprint.costs.ore && materials.clay >= blueprint.costs.clay && materials.obsidian >= blueprint.costs.obsidian)
+                if (state.material.ore >= blueprint.costs.ore && state.material.clay >= blueprint.costs.clay && state.material.obsidian >= blueprint.costs.obsidian)
                 {
-                    maxGeode = Math.Max(maxGeode, 
-                        solve(bi, time + 1,
-                        (   materialsNext.ore - blueprint.costs.ore, 
-                            materialsNext.clay - blueprint.costs.clay, 
-                            materialsNext.obsidian - blueprint.costs.obsidian, 
-                            materialsNext.geode), 
-                        (   production.ore + blueprint.produces.ore, 
-                            production.clay + blueprint.produces.clay, 
-                            production.obsidian + blueprint.produces.obsidian, 
-                            production.geode + blueprint.produces.geode)));
+                    maxGeode = Math.Max(maxGeode, Solve(MAXMINUTES, stateCache, blueprints, nextState with { material = nextState.material.Subtract(blueprint.costs), production = nextState.production.Add(blueprint.production) }));
                 }
             }
 
-            maxGeode = Math.Max(maxGeode, 
-                solve(bi, time + 1, materialsNext, production));
+            maxGeode = Math.Max(maxGeode, Solve(MAXMINUTES, stateCache, blueprints, nextState));
             
-            state[(time, materials, production)] = maxGeode;
+            stateCache[state] = maxGeode;
             return maxGeode;
         }
     }
