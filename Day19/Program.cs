547d433ab80155564e3e1a8d29d160f52321c236
Christian <cwaters@truenorthit.co.uk>
2022-12-20 22:48:51 +0000

    now deciding on next robot to build, rather than minute by minute

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -12,10 +12,12 @@ namespace Day19
             public Material Subtract(Material other) => new Material(ore - other.ore, clay - other.clay, obsidian - other.obsidian);
             public Material Multiply(int multiple) => new Material(ore * multiple, clay * multiple, obsidian * multiple);
         }
-
-        record struct State(int time, Material material, Material production, Material lastProduced, int currentScore);
         
-        static List<List<(Material costs, Material produces, bool isGeode)>> blueprintSets;
+        record struct Blueprint(Material costs, Material production, bool isGeode);
+
+        record struct State(int time, Material material, Material production, int currentScore, Blueprint toBuild);
+        
+        static List<List<Blueprint>> blueprintSets;
 
         static int best;
 
@@ -26,11 +28,11 @@ namespace Day19
             
             blueprintSets = File.ReadAllLines($"{args[0]}.txt")
                 .Select(str => str.Split(' '))
-                .Select(    arr => new List<(Material costs, Material production, bool isGeode)>() {
-                                     (new Material(int.Parse(arr[6]), 0, 0), new Material(1,0,0), false),
-                                     (new Material(int.Parse(arr[12]), 0, 0), new Material(0,1,0), false),
-                                     (new Material(int.Parse(arr[18]), int.Parse(arr[21]), 0), new Material(0,0,1), false),
-                                     (new Material(int.Parse(arr[27]),0, int.Parse(arr[30])), new Material(0,0,0), true),
+                .Select(    arr => new List<Blueprint>() {
+                                     new Blueprint(new Material(int.Parse(arr[27]),0, int.Parse(arr[30])), new Material(0,0,0), true),
+                                     new Blueprint(new Material(int.Parse(arr[18]), int.Parse(arr[21]), 0), new Material(0,0,1), false),
+                                     new Blueprint(new Material(int.Parse(arr[12]), 0, 0), new Material(0,1,0), false),
+                                     new Blueprint(new Material(int.Parse(arr[6]), 0, 0), new Material(1,0,0), false),
                             }).ToList();
 
             var part1Answers = RunPart(24, blueprintSets);
@@ -43,30 +45,28 @@ namespace Day19
             Console.WriteLine($"Part 2: {part2}, elapsed {watch.ElapsedMilliseconds}ms");
         }
 
-        static List<(int index, int geodes)> RunPart(int MAXMINUTES, IEnumerable<List<(Material costs, Material produces, bool isGeode)>> blueprintSets)
+        static List<(int index, int geodes)> RunPart(int MAXMINUTES, IEnumerable<List<Blueprint>> blueprintSets)
         {
             best = 0;
             var answers = new List<(int, int)>();
+            var emptyBlueprint = new Blueprint(new Material(0, 0, 0), new Material(0, 0, 0), false);
             foreach ((var blueprints, int i) in blueprintSets.Select((bp, i) => (bp, i)))
             {
-                var answer = Solve(MAXMINUTES, new Dictionary<State, int>(), blueprints, new State(1, new Material(0, 0, 0), new Material(1, 0, 0), new Material(0, 0, 0), 0));
-                Console.WriteLine($"Blueprint {i}: {answer}");
+                //note we start with 1 ore, as this is the material AFTER the minute being considered
+                var answer = Solve(MAXMINUTES, new Dictionary<State, int>(), blueprints, new State(1, new Material(0, 0, 0), new Material(1, 0, 0), 0, emptyBlueprint));
+                Console.WriteLine($"Blueprint {i + 1}: {answer}");
                 answers.Add((i + 1, answer));
             }
 
             return answers;
         }
 
-        static int Solve(int MAXMINUTES, Dictionary<State, int> stateCache, List<(Material costs, Material production, bool isGeode)> blueprints, State state)
+        static int Solve(int MAXMINUTES, Dictionary<State, int> stateCache, List<Blueprint> blueprints, State state)
         {
             if (stateCache.TryGetValue(state, out int result))
             {
                 return result;
             }
-
-            //state.material is the material we have at the start of this minute (after building whatever robot we build this minute)
-            //state.production is the production we will have THIS MINUTE
-            //state.currentScore is the final value (at MAXMINUTES of any geodes we have already built
             
             int maxGeode = state.currentScore;
 
@@ -76,35 +76,40 @@ namespace Day19
             {
                 //we have ALWAYS built a robot this turn (except the first turn)
                 //so minimum time until we can build next robot is next turn, or the first turn when we have sufficient material
-                var timeForOre = Math.Ceiling((blueprint.costs.ore - state.material.ore) / (state.production.ore == 0 ? 0.001m  : state.production.ore));
-                var timeForClay = Math.Ceiling((blueprint.costs.clay - state.material.clay) / (state.production.clay == 0 ? 0.001m : state.production.clay));
-                var timeForObsidian = Math.Ceiling((blueprint.costs.obsidian - state.material.obsidian) / (state.production.obsidian == 0 ? 0.001m : state.production.obsidian));
 
-                if (timeForOre <= 0) timeForOre = 1;
-                if (timeForClay <= 0) timeForClay = 1;
-                if (timeForObsidian <= 0) timeForObsidian = 1;
+                //e.g. we have 1 ore, with 1 ore production (next turn) and it costs 4 ore to build
+                var needed = blueprint.costs.Subtract(state.material).Add(state.toBuild.costs);
+                var neededNextMinute = needed.Subtract(state.production);
+                var productionNextMinute = state.production.Add(state.toBuild.production);
+                
+                var timeForOre = needed.ore <= 0 ? 1 : productionNextMinute.ore == 0 ? MAXMINUTES :
+                    Math.Ceiling((decimal)neededNextMinute.ore / productionNextMinute.ore) + 1;
+
+                var timeForClay = needed.clay <= 0 ? 1 : productionNextMinute.clay == 0 ? MAXMINUTES :
+                    Math.Ceiling((decimal)neededNextMinute.clay / productionNextMinute.clay) + 1;
+
+                var timeForObsidian = needed.obsidian <= 0 ? 1 : productionNextMinute.obsidian == 0 ? MAXMINUTES :
+                    Math.Ceiling((decimal)neededNextMinute.obsidian / productionNextMinute.obsidian) + 1;
 
                 var nextTime = (int) Math.Max(Math.Max(timeForOre, timeForClay), timeForObsidian);
 
-                //any we can build immediately have already been dealt with
-                //so only consider FUTURE robots
-                if (nextTime > 0 && state.time + nextTime < MAXMINUTES)
+                //state.material is the material we have at the start of this minute
+                //toBuild is the blueprint we will build
+                //state.production is the existing production we have
+                //state.currentScore is the final value (at MAXMINUTES) of any geodes we have already built
+                if (state.time + nextTime < MAXMINUTES)
                 {
                     maxGeode = Math.Max(maxGeode, Solve(MAXMINUTES, stateCache, blueprints, 
                         state with { 
                             time = state.time + nextTime, 
-                            material = state.material.Add(state.production.Multiply(nextTime)).Subtract(blueprint.costs), 
-                            production = state.production.Add(state.lastProduced),
+                            material = state.material.Add(state.production).Add(state.production.Add(state.toBuild.production).Multiply(nextTime - 1)).Subtract(state.toBuild.costs), 
+                            production = state.production.Add(state.toBuild.production),
                             currentScore = state.currentScore + (blueprint.isGeode ? (MAXMINUTES - state.time - nextTime) : 0),
-                            lastProduced = blueprint.production
+                            toBuild = blueprint
                         }));
                 }
             }
 
-            if (maxGeode > best)
-            {
-                best = maxGeode;
-            }
             stateCache[state] = maxGeode;
             return maxGeode;
         }
