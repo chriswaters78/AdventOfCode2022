672d40a95aaf3a233694b79d1fe1674e341487dc
Christian <cwaters@truenorthit.co.uk>
2022-12-21 13:43:21 +0000

    part 2

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using System.IO;
+using System.Numerics;
+using static Day21.Program;
 
 namespace Day21
 {
@@ -19,78 +21,104 @@ namespace Day21
         {
             public BigInteger Value;
         }
-
+        static Dictionary<string, Node> nodes;
         static void Main(string[] args)
         {
-            List<Node> nodes = new List<Node>();
+            nodes = new Dictionary<string, Node>();
             foreach (var arr in File.ReadAllLines($"{args[0]}.txt").Select(str => str.Split(": ")))
             {
                 if (BigInteger.TryParse(arr[1], out BigInteger value))
                 {
-                    nodes.Add(new VNode() { Name = arr[0], Value = value });
+                    nodes.Add(arr[0], new VNode() { Name = arr[0], Value = value });
                 }
                 else
                 {
                     var op = arr[1].Split(' ');
-                    nodes.Add(new CNode() { Name = arr[0], Operation = op[1][0], Node1 = op[0], Node2 = op[2] });
+                    nodes.Add(arr[0], new CNode() { Name = arr[0], Operation = op[1][0], Node1 = op[0], Node2 = op[2] });
                 }
             }
 
+            var values1 = solve(true);
+            var part1 = values1["root"].nominator / values1["root"].denominator;
+            Console.WriteLine($"Part 1: {part1}");
 
+            var values2 = solve(false);
+            Console.WriteLine($"Part 2, solve this equation (https://www.mathpapa.com/equation-solver/)");
+            Console.WriteLine(getEquation(values2, "root"));
+
+            var part2 = 801541236563053696 / 232925;
+        }
+
+        private static Dictionary<string, (BigInteger nominator, BigInteger denominator)> solve(bool part1)
+        {
             var values = new Dictionary<string, (BigInteger nominator, BigInteger denominator)>();
-            while (values.Count < nodes.Count)
+            bool noHits;
+            do
             {
-                foreach (var node in nodes.Where(node => !values.ContainsKey(node.Name)))
+                noHits = true;
+                foreach (var node in nodes.Values.Where(node => !values.ContainsKey(node.Name) && (part1 || node.Name != "humn")))
                 {
                     if (node is VNode)
                     {
                         values.Add(node.Name, (((VNode)node).Value, 1));
+                        noHits = false;
                     }
                     else
                     {
                         var cnode = node as CNode;
                         if (values.ContainsKey(cnode.Node1) && values.ContainsKey(cnode.Node2))
                         {
-                            if (cnode.Name == "root")
-                            {
-                            }
+                            noHits = false;
+                            BigInteger nominator;
+                            BigInteger denominator;
                             switch (cnode.Operation)
                             {
                                 case '+':
-                                    {
-                                        var nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator + values[cnode.Node2].nominator * values[cnode.Node1].denominator;
-                                        var denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
-                                        values.Add(cnode.Name, (nominator, denominator));
-                                    }
+                                    nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator + values[cnode.Node2].nominator * values[cnode.Node1].denominator;
+                                    denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
                                     break;
                                 case '-':
-                                    {
-                                        var nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator - values[cnode.Node2].nominator * values[cnode.Node1].denominator;
-                                        var denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
-                                        values.Add(cnode.Name, (nominator, denominator));
-                                    }
+                                    nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator - values[cnode.Node2].nominator * values[cnode.Node1].denominator;
+                                    denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
                                     break;
                                 case '*':
-                                    {
-                                        var nominator = values[cnode.Node1].nominator * values[cnode.Node2].nominator;
-                                        var denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
-                                        values.Add(cnode.Name, (nominator, denominator));
-                                    }
+                                    nominator = values[cnode.Node1].nominator * values[cnode.Node2].nominator;
+                                    denominator = values[cnode.Node1].denominator * values[cnode.Node2].denominator;
                                     break;
-                                case '/':
-                                    {
-                                        var nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator;
-                                        var denominator = values[cnode.Node1].denominator * values[cnode.Node2].nominator;
-                                        values.Add(cnode.Name, (nominator, denominator));
-                                    }
+                                default:
+                                    nominator = values[cnode.Node1].nominator * values[cnode.Node2].denominator;
+                                    denominator = values[cnode.Node1].denominator * values[cnode.Node2].nominator;
                                     break;
                             }
+                            var gcd = BigInteger.GreatestCommonDivisor(nominator, denominator);
+                            values.Add(cnode.Name, (nominator / gcd, denominator / gcd));
                         }
                     }
                 }
-            }
+            } while (!noHits);
 
-            var part1 = values["root"].nominator / values["root"].denominator;
+            return values;
+        }
+
+        private static string getEquation(Dictionary<string, (BigInteger nominator, BigInteger denominator)> values, string nodeName)
+        {
+            if (values.ContainsKey(nodeName))
+            {
+                if (values[nodeName].denominator != 1)
+                {
+                    throw new Exception($"Denominator not 1!");
+                }
+                return values[nodeName].nominator.ToString();
+            }
+            else
+            {
+                if (nodeName == "humn")
+                {
+                    return "h";
+                }
+                var node = nodes[nodeName] as CNode;
+                return $"({getEquation(values, node.Node1)}) {(nodeName == "root" ? "=" : node.Operation)} ({getEquation(values, node.Node2)})";
+            }
         }
     }
 }
