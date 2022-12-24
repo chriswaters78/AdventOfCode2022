49851e836b953065329bc5166522aa9798b6acda
Unknown <cwaters@truenorthit.co.uk>
2022-12-24 15:21:21 +0000

    optimised

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -6,30 +6,72 @@ namespace Day24
 {
     internal class Program
     {
-        record struct State(int r, int c, int t, int p);
+        record struct State((int r, int c) position, int t);
 
         static HashSet<(int r, int c)>[] grids;
-        static Dictionary<State, int> cache = new Dictionary<State, int>();
         static int R;
         static int C;
         static int LCM;
 
         static void Main(string[] args)
         {
-            //input is 120 x 25
-            //2 x 2 x 2 x 3 x 5
-            // 5 x 5
-            //LCM = 600
-
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            var lines = File.ReadAllLines($"{args[0]}.txt");
+            grids = generateBlizzardGrids(args[0]);
+
+            var part1 = solve(new State((-1, 0), 0), (R, C - 1));
+            var endToStart = solve(new State((R, C - 1), part1), (-1, 0));
+            var part2 = solve(new State((-1, 0), endToStart), (R, C-1));
+
+            Console.WriteLine($"Part 1: {part1}");
+            Console.WriteLine($"Part 2: {part2}");
+            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
+        }
+
+        private static int solve(State initialState, (int r, int c) goal)
+        {
+            var cache = new Dictionary<State, int>();
+            var queue = new Queue<State>();
+            queue.Enqueue(initialState);
+
+            while (queue.Any())
+            {
+                var state = queue.Dequeue();
+
+                var cacheKey = state with { t = state.t % LCM };
+                if (cache.TryGetValue(cacheKey, out int result) && state.t >= result)
+                {
+                    continue;
+                }
+
+                cache[cacheKey] = state.t;
+                if (state.position.r == goal.r && state.position.c == goal.c)
+                {
+                    return state.t;
+                }
+
+                foreach ((int dr, int dc) offset in new[] { (1, 0), (0, 1), (0, 0), (-1, 0), (0, -1) })
+                {
+                    var newState = state with { t = state.t + 1, position = (state.position.r + offset.dr, state.position.c + offset.dc) };
+                    if (!grids[(state.t + 1) % LCM].Contains(newState.position))
+                    {
+                        queue.Enqueue(newState);
+                    }
+                }
+            }
+
+            throw new Exception($"Unsolvable!");
+        }
+
+        private static HashSet<(int r, int c)>[] generateBlizzardGrids(string input)
+        {
+            var lines = File.ReadAllLines($"{input}.txt");
             R = lines.Length - 2;
             C = lines.First().Length - 2;
             LCM = R * C / (int)BigInteger.GreatestCommonDivisor(R, C);
 
-            grids = new HashSet<(int r, int c)>[LCM];
+            var grids = new HashSet<(int r, int c)>[LCM];
 
             for (int t = 0; t < LCM; t++)
             {
@@ -41,31 +83,27 @@ namespace Day24
                         switch (lines[r + 1][c + 1])
                         {
                             case '<':
-                                //move LCM - i places to right
                                 grids[t].Add((r, (c + LCM - t) % C));
                                 break;
                             case '>':
-                                //move t places to right
                                 grids[t].Add((r, (c + t) % C));
                                 break;
                             case 'v':
-                                //move t places down
                                 grids[t].Add(((r + t) % R, c));
                                 break;
                             case '^':
-                                //move LCM - t t places up
                                 grids[t].Add(((r + LCM - t) % R, c));
                                 break;
                         }
                     }
                 }
 
-                for (int r = -2; r < R+2; r++)
+                for (int r = -2; r < R + 2; r++)
                 {
                     grids[t].Add((r, -1));
                     grids[t].Add((r, C));
                 }
-                for (int c = -1; c < C+1; c++)
+                for (int c = -1; c < C + 1; c++)
                 {
                     grids[t].Add((-1, c));
                     grids[t].Add((-2, c));
@@ -76,30 +114,7 @@ namespace Day24
                 grids[t].Remove((R, C - 1));
             }
 
-            //Console.WriteLine(print(grids[0], new State(-1, 0, 0, 0)));
-            //Console.WriteLine();
-            //Console.WriteLine(print(grids[1], new State(-1, 0, 0, 0)));
-
-            solve(new State(-1, 0, 0, 0));
-            int best1 = int.MaxValue;
-            int best2 = int.MaxValue;
-            for (int t = 0; t < LCM; t++)
-            {
-                var finalState1 = new State(R, C - 1, t, 1);
-                var finalState2 = new State(R, C - 1, t, 2);
-                if (cache.ContainsKey(finalState1))
-                {
-                    best1 = Math.Min(best1, cache[finalState1]);
-                }
-                if (cache.ContainsKey(finalState2))
-                {
-                    best2 = Math.Min(best2, cache[finalState2]);
-                }
-            }
-
-            Console.WriteLine($"Part 1: {best1}");
-            Console.WriteLine($"Part 2: {best2}");
-            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
+            return grids;
         }
 
         static string print(HashSet<(int r, int c)> grid, State state)
@@ -111,7 +126,7 @@ namespace Day24
                 sbs.Add(thisRow);
                 for (int c = -1; c <= C; c++)
                 {
-                    if (state.r == r && state.c == c)
+                    if (state.position.r == r && state.position.c == c)
                     {
                         thisRow.Append('E');
                     }
@@ -123,78 +138,6 @@ namespace Day24
             }
 
             return String.Join(Environment.NewLine, sbs.Select(sb => sb.ToString()));
-        }
-
-        //for every possible starting t
-        //do a DFS tracking best state to given position
-        private static void solve(State initialState)
-        {
-            Queue<State> queue = new Queue<State>();
-            queue.Enqueue(initialState);
-
-            while (queue.Any())
-            {
-                var state = queue.Dequeue();
-
-                //Console.WriteLine($"T={state.t}");
-                //Console.WriteLine(print(grids[state.t % LCM], state));
-
-                if (state.r == R && state.c == C - 1)
-                {
-                    if (state.p == 0)
-                    {
-                        state = state with { p = 1 };
-                    }
-                }
-                if (state.r == -1 && state.c == 0)
-                {
-                    if (state.p == 1)
-                    {
-                        state = state with { p = 2 };
-                    }
-                }
-                var cacheKey = state with { t = state.t % LCM };
-                if (cache.TryGetValue(cacheKey, out int result))
-                {
-                    if (state.t >= result)
-                    {
-                        continue;
-                    }
-                }
-
-                cache[cacheKey] = state.t;
-                if (state.r == R && state.c == C - 1 && state.p == 2)
-                {
-                    continue;
-                }
-
-                var nextGrid = grids[(state.t + 1) % LCM];
-                //down
-                if (!nextGrid.Contains((state.r + 1, state.c)))
-                {
-                    queue.Enqueue(state with { t = state.t + 1, r = state.r + 1 });
-                }
-                //right
-                if (!nextGrid.Contains((state.r, state.c + 1)))
-                {
-                    queue.Enqueue(state with { t = state.t + 1, c = state.c + 1 });
-                }
-                //wait
-                if (!nextGrid.Contains((state.r, state.c)))
-                {
-                    queue.Enqueue(state with { t = state.t + 1 });
-                }
-                //up
-                if (!nextGrid.Contains((state.r - 1, state.c)))
-                {
-                    queue.Enqueue(state with { t = state.t + 1, r = state.r - 1 });
-                }
-                //left
-                if (!nextGrid.Contains((state.r, state.c - 1)))
-                {
-                    queue.Enqueue(state with { t = state.t + 1, c = state.c - 1 });
-                }
-            }
         }
     }
 }
