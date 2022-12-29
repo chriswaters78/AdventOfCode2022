a22256071ee8c2aa39c4d6752909f7d7e8abc045
Unknown <cwaters@truenorthit.co.uk>
2022-12-29 22:16:23 +0000

    bug in handling time

---
+++ Day16_Fast/Program.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Day16
+{
+    internal class Program
+    {
+        static Dictionary<int, (int, List<int>)> indexGraph;
+        static int[,] allPairs;
+
+        const int MINUTES = 26;
+        const int MAX = 10000;
+
+        static void Main(string[] args)
+        {
+            var stringGraph = File.ReadAllLines($"{args[0]}.txt")
+                .Select(
+                        str => (str[6..8],
+                        int.Parse(str[23..25].Trim(';')),
+                        (str.Contains("valves") ? str.Split("valves ")[1] : str.Split("valve ")[1]).Split(", ").ToArray()
+                    )).ToDictionary(tp => tp.Item1, tp => (tp.Item2, tp.Item3));
+
+            var nonZeroCount = stringGraph.Where(kvp => kvp.Value.Item1 != 0).Count();
+
+            var stringToIndex = stringGraph.OrderByDescending(kvp => kvp.Value.Item1).Select((kvp, i) => (kvp.Key, i)).ToDictionary(tp => tp.Key, tp => tp.i);
+            var indexToString = stringToIndex.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+            indexGraph = stringGraph.ToDictionary(kvp => stringToIndex[kvp.Key], kvp => (kvp.Value.Item1, kvp.Value.Item2.Select(str => stringToIndex[str]).ToList()));
+
+            //partition our valve sets into disjoint sets
+            var sets = new List<(List<int>, List<int>)>();
+            for (ulong i = 0; i < Math.Pow(2, nonZeroCount); i++)
+            {
+                var list1 = new List<int>();
+                var list2 = new List<int>();
+                for (int b = 0; b < nonZeroCount; b++)
+                {
+                    if (IsBitSet(b, i))
+                    {
+                        list1.Add(b);
+                    }
+                    else
+                    {
+                        list2.Add(b);
+                    }
+                }
+                sets.Add((list1, list2));
+            }
+            Console.WriteLine($"{sets.Count} disjoint sets found");
+
+            //get all pairs shortest paths using Floyd-Warshall
+            //note this is the DISTANCE only
+            //then for each disjoint item, recursively each side of the set, starting at AA
+            //and find the maximum pressure relieved within the time limit
+            //the maximum of all of these is the answer
+            allPairs = FloydWarshall(indexGraph);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int best = 0;
+            foreach (var pair in sets)
+            {
+                Console.WriteLine($"Testing set: [{String.Join(", ", pair.Item1)}] and [{String.Join(", ", pair.Item2)}]");
+                //solve for each side and add answers
+                var set1Best = solve(pair.Item1, 0, 0, 0, stringToIndex["AA"]);
+                Console.WriteLine($"Set 1: {set1Best}");
+                var set2Best = solve(pair.Item2, 0, 0, 0, stringToIndex["AA"]);
+                Console.WriteLine($"Set 2: {set2Best}");
+                best = Math.Max(best, set1Best + set2Best);
+            }
+
+            Console.WriteLine($"Part2: {best} in {watch.ElapsedMilliseconds}ms");
+        }
+
+        static bool IsBitSet(int bit, ulong state)
+        {
+            return (state & (1UL << bit)) != 0;
+        }
+        static ulong SetBit(int bit, ulong state)
+        {
+            return state |= 1UL << bit;
+        }
+
+        //static int maxFlowLeft(ulong state)
+        //{
+        //    int flow = 0;
+        //    for (int i = 0; i < graph2.Count; i++)
+        //    {
+        //        if (!IsBitSet(i, state))
+        //        {
+        //            flow += graph2[i].flow;
+        //        }
+        //    }
+
+        //    return flow;
+        //}
+
+        public static int[,] FloydWarshall(Dictionary<int, (int, List<int>)> graph)
+        {
+            int[,] distance = new int[graph.Count, graph.Count];
+            for (int i = 0; i < graph.Count; i++)
+            {
+                for (int j = 0; j < graph.Count; j++)
+                {
+                    distance[i, j] = MAX;
+                }
+            }
+
+            foreach (var key in graph.Keys)
+            {
+                foreach (var edge in graph[key].Item2)
+                {
+                    distance[key, edge] = 1;
+                }
+            }
+
+            for (int k = 0; k < graph.Count; ++k)
+            {
+                for (int i = 0; i < graph.Count; ++i)
+                {
+                    for (int j = 0; j < graph.Count; ++j)
+                    {
+                        if (distance[i, k] + distance[k, j] < distance[i, j])
+                            distance[i, j] = distance[i, k] + distance[k, j];
+                    }
+                }
+            }
+
+            return distance;
+        }
+
+        static int solve(List<int> toOpen, int timespent, int currentflow, ulong valves, int currentValve)
+        {
+            //we are trying to open all valves in toOpen
+            //we have opened (and added the final score) for all valves with bits set in valves
+            //we have to try each valve remaining in toOpen
+
+            int best = currentflow;
+            foreach (var nextValve in toOpen)
+            {
+                if (!IsBitSet(nextValve, valves))
+                {
+                    //try opening this one next
+                    var timeToValve = allPairs[currentValve, nextValve];
+                    if (timeToValve + timespent < MINUTES)
+                    {
+                        //we can reach AND open it
+                        int nextFlow = currentflow + (MINUTES - (timeToValve + timespent + 1)) * indexGraph[nextValve].Item1;
+                        var nextValves = SetBit(nextValve, valves);
+                        int nextBest = solve(toOpen, timespent + timeToValve, nextFlow, nextValves, nextValve);
+                        best = Math.Max(best, nextBest);
+                    }
+                }
+            }
+
+            return best;
+
+            //if (maxFlowLeft(valves) * (MINUTES - timespent) + currentflow < best)
+            //{
+            //    //not possible to improve from here
+            //    return;
+            //}
+
+        }
+    }
+}
--- Day25/Program.cs
+++ Day25/Program.cs
@@ -1,45 +1,11 @@
-﻿using System.Diagnostics;
-using System.Numerics;
-using System.Text;
+﻿using System.Text;
 
 namespace Day25
 {
     internal class Program
     {
-
-        static (int, string)[] tests = new[]
-        {
-            (1,"1"),
-            (2,"2"),
-            (3,"1="),
-            (4,"1-"),
-            (5,"10"),
-            (6,"11"),
-            (7,"12"),
-            (8,"2="),
-            (9,"2-"),
-            (10,"20"),
-            (15,"1=0"),
-            (20,"1-0"),
-            (2022,"1=11-2"),
-            (12345,"1-0---0"),
-            (314159265,"1121-1110-1=0"),
-        };
-
         static void Main(string[] args)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            foreach ((int dec, string elf) in tests)
-            {
-                var converted = convertToElf(dec);
-                if (converted != elf)
-                {
-                    throw new Exception($"Boom: {dec} was converted to {converted} instead of {elf}");
-                }
-            }
-
             var elfNumbers = File.ReadAllLines($"{args[0]}.txt");
             var decimalNumbers = new List<long>();
             foreach (var elf in elfNumbers)
@@ -62,56 +28,47 @@ namespace Day25
                 decimalNumbers.Add(number);
             }
 
-            var sum = decimalNumbers.Sum();
-
-
-
-            var part1 = convertToElf(sum);
+            var part1 = convertToElf(decimalNumbers.Sum());
             Console.WriteLine($"Part 1: {part1}");
-            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
         }
 
         private static string convertToElf(long number)
         {
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
+            long units = 5;
+            while (true)
             {
-                long units = 1;
-                while (true)
+                var rem = number % (units * 5) / units;
+                var div = number / (units * 5);
+                sb.Append(rem.ToString());
+                if (div == 0)
                 {
-                    var rem = number % (units * 5) / units;
-                    var div = number / (units * 5);
-                    sb.Append(rem.ToString());
-                    if (div == 0)
-                    {
-                        break;
-                    }
-                    units *= 5;
+                    break;
                 }
+                units *= 5;
             }
 
-            var sumDigits = sb.ToString().Select(ch => int.Parse(ch.ToString()));
-
-            int carry = 0;
-            List<int> digits = new List<int>();
-            foreach (var digit in sumDigits)
+            bool carry = false;
+            var digits = new List<int>();
+            foreach (var digit in sb.ToString().Select(ch => int.Parse(ch.ToString())))
             {
-                var newDigit = digit + carry;
-                carry = 0;
+                var newDigit = digit + (carry ? 1 : 0);
+                carry = false;
 
                 if (newDigit >= 3)
                 {
-                    carry = 1;
+                    carry = true;
                     newDigit -= 5;
                 }
                 digits.Add(newDigit);
             }
-            if (carry == 1)
+            if (carry)
             {
                 digits.Add(1);
             }
 
             digits.Reverse();
-            StringBuilder answer = new StringBuilder();
+            var answer = new StringBuilder();
             foreach (var digit in digits)
             {
                 answer.Append(digit switch
@@ -121,12 +78,10 @@ namespace Day25
                     0 => '0',
                     1 => '1',
                     2 => '2',
-                    _ => throw new Exception($"BOOM!"),
                 });
             }
 
             return answer.ToString();
         }
-
     }
 }
